23456465a5fcb28c1e7d387e77e3710e1e0bdb96
Robert Echeverria <failquality@gmail.com>
2014-11-29 15:29:23 -0800

    What I have so far for cs480 Project

---
+++ PTWinMobileApp/MainMenu.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
+
+namespace PTWinMobileApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainMenu : Page
+    {
+        public MainMenu()
+        {
+            this.InitializeComponent();
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+
+        }
+
+        public void NewFormClicked(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(PTForm));
+        }
+
+        public void RecentFormClicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public void SearchFormClicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public void LogOutClicked(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Login));
+        }
+
+        private void BackKeyPress(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+    }
+}
---
+++ PTWinMobileApp/PTForm.xaml.cs
@@ -0,0 +1,57 @@
+﻿using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
+
+namespace PTWinMobileApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class PTForm : Page
+    {
+        public PTForm()
+        {
+            this.InitializeComponent();
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+            Windows.Phone.UI.Input.HardwareButtons.BackPressed += HardwareButtons_BackPressed;
+
+
+        }
+
+        public void HardwareButtons_BackPressed(object sender, Windows.Phone.UI.Input.BackPressedEventArgs e)
+        {
+            if (this.Frame.CanGoBack)
+            {
+                // Clear the status block when navigating 
+                //NotifyUser(String.Empty, NotifyType.StatusMessage);
+
+                this.Frame.GoBack();
+
+                //Indicate the back button press is handled so the app does not exit 
+                e.Handled = true;
+            }
+        }
+
+        public void NextStepForm2Clicked(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(PTForm2));
+        }
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+
+    }
+}
